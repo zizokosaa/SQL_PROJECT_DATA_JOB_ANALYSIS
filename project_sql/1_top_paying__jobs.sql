b9371434e1239327cc1fb76f11bf9a354b0d15f8
zizokosaa <136484183+zizokosaa@users.noreply.github.com>
2024-04-19 17:44:24 +0200

    upload sql files

--- project_sql/1_top_paying__jobs.sql
+++ project_sql/1_top_paying__jobs.sql
@@ -14,6 +14,12 @@ SELECT
     job_location,
     job_schedule_type,
     salary_year_avg,
-    job_posted_date
-FROM    
-    job_postings_fact
+    job_posted_date,
+    name AS company_name
+FROM  job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE job_title_short = 'Data Scientist' AND 
+      job_location = 'Anywhere' AND
+      salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+LIMIT 10;
---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,10 @@
+/*
+Question: what are the most in-demand skills for data Science?
+*/
+
+
+SELECT * 
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+LIMIT 10;
